f4cb414eae285c9a767a9fc6015201978d0c563d
Julien Fournier <fournier.j@sfr.fr>
2018-01-05 10:53:30 +0100

    Repudiation endpoint fetch

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-var utils = require("./lib/utils");
+const utils = require('./lib/utils')
 
 function Mango(options) {
-  if (!(this instanceof Mango)) return new Mango(options);
+  if (!(this instanceof Mango)) { return new Mango(options) }
 
-  this._options = options || {};
+  this._options = options || {}
 
   this._api = {
     auth: false,
-    protocol: "https",
-    host: "api.sandbox.mangopay.com",
-    port: "",
-    basePath: "",
-    version: this._options.version || "v2",
-    timeout: 120 * 1000
-  };
+    protocol: 'https',
+    host: 'api.sandbox.mangopay.com',
+    port: '',
+    basePath: '',
+    version: this._options.version || 'v2',
+    timeout: 120 * 1000,
+  }
 
   // set base path
   this._api.basePath = [
-    "/",
+    '/',
     this._api.version,
-    "/",
-    this._options.username
-  ].join("");
+    '/',
+    this._options.username,
+  ].join('')
 
   // switch to production url
-  if (this._options.production) this._api.host = "api.mangopay.com";
+  if (this._options.production) { this._api.host = 'api.mangopay.com' }
 
   // mount endpoints
   this.mount(
     this._options.mount ||
-      "user|card|kyc|wallet|author|bank|document|payin|hook|event|mandate"
-  );
+      'user|card|kyc|wallet|author|bank|document|payin|hook|event|mandate|repudiation'
+  )
 
   // set auth
-  this.setAuth(options);
+  this.setAuth(options)
 }
 
 Mango.prototype = {
-  setAuth: function(key) {
+  setAuth(key) {
     if (utils.isAuthKey(key))
-      this._api.auth =
-        "Basic " +
-        new Buffer(key.username + ":" + key.password).toString("base64");
-    else throw Error("Provide credentials");
+    { this._api.auth =
+        `Basic ${
+          new Buffer(`${key.username  }:${  key.password}`).toString('base64')}` }
+    else { throw Error('Provide credentials') }
   },
 
-  mount: function(resources) {
-    var self = this;
-    resources.split("|").forEach(function(resource) {
-      self[resource] = new (require("./resources/" + resource))(self);
-    });
-  }
-};
+  mount(resources) {
+    const self = this
+    resources.split('|').forEach((resource) => {
+      self[resource] = new (require(`./resources/${  resource}`))(self)
+    })
+  },
+}
 
-module.exports = Mango;
+module.exports = Mango
