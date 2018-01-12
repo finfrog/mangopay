7d25a31ac60c11c486fe43e735193ce195d20b62
Julien Fournier <fournier.j@sfr.fr>
2018-01-12 10:15:28 +0100

    pagination can now be modified on user transactions

--- resources/user.js
+++ resources/user.js
@@ -1,37 +1,36 @@
-
-var httpClient = require('../lib/httpClient')
-  , httpMethod = require('../lib/httpMethod')
+let httpClient = require('../lib/httpClient'),
+  httpMethod = require('../lib/httpMethod')
 
 module.exports = httpClient.extend({
-  
   path: 'users',
-  
-  includeBasic: [ 'list' ],
+
+  includeBasic: ['list'],
 
   methods: {
-
     // By default we are in the "Natural User Context"
     create: httpMethod({
       method: 'POST',
       path: 'natural',
       params: {
-          'Email': { required: true }
-        , 'FirstName': { required: true }
-        , 'LastName': { required: true }
-        , 'Birthday': { required: true }
-        , 'Nationality': { required: true, default: 'FR' }
-        , 'CountryOfResidence': { required: true, default: 'FR' }
-      }
+        Email: { required: true },
+        FirstName: { required: true },
+        LastName: { required: true },
+        Birthday: { required: true },
+        Nationality: { required: true, default: 'FR' },
+        CountryOfResidence: { required: true, default: 'FR' },
+      },
     }),
 
     // Create a user and his wallet
-    signup: function(user, callback){
-      var self = this
+    signup(user, callback) {
+      const self = this
 
-      var createWallet = function(err, user){
-        if(err) return callback(err)
+      const createWallet = function(err, user) {
+        if (err) {
+          return callback(err)
+        }
 
-        self._root.wallet.create({ Owners: [user.Id] }, function(err, wallet){
+        self._root.wallet.create({ Owners: [user.Id] }, (err, wallet) => {
           // return user & wallet
           callback(err, user, wallet)
         })
@@ -43,13 +42,13 @@ module.exports = httpClient.extend({
     fetch: httpMethod({
       method: 'GET',
       path: 'natural/{Id}',
-      params: { 'Id': { required: true } }
+      params: { Id: { required: true } },
     }),
 
     update: httpMethod({
       method: 'PUT',
       path: 'natural/{Id}',
-      params: { 'Id': { required: true } }
+      params: { Id: { required: true } },
     }),
 
     // Legal User
@@ -57,63 +56,68 @@ module.exports = httpClient.extend({
       method: 'POST',
       path: 'legal',
       params: {
-          'Email': { required: true }
-        , 'Name': { required: true }
-        , 'LegalPersonType': { required: true }
-        , 'LegalRepresentativeFirstName': { required: true }
-        , 'LegalRepresentativeLastName': { required: true }
-        , 'LegalRepresentativeBirthday': { required: true }
-        , 'LegalRepresentativeNationality': { required: true, default: 'FR' }
-        , 'LegalRepresentativeCountryOfResidence': { required: true, default: 'FR' }
-      }
+        Email: { required: true },
+        Name: { required: true },
+        LegalPersonType: { required: true },
+        LegalRepresentativeFirstName: { required: true },
+        LegalRepresentativeLastName: { required: true },
+        LegalRepresentativeBirthday: { required: true },
+        LegalRepresentativeNationality: { required: true, default: 'FR' },
+        LegalRepresentativeCountryOfResidence: {
+          required: true,
+          default: 'FR',
+        },
+      },
     }),
 
     fetchLegal: httpMethod({
       method: 'GET',
       path: 'legal/{Id}',
-      params: { 'Id': { required: true } }
+      params: { Id: { required: true } },
     }),
 
     updateLegal: httpMethod({
       method: 'PUT',
       path: 'legal/{Id}',
-      params: { 'Id': { required: true } }
+      params: { Id: { required: true } },
     }),
 
     cards: httpMethod({
       method: 'GET',
       path: '{UserId}/cards?per_page={per_page}&page={page}',
       params: {
-         'UserId': { required: true },
-         'per_page': { required: false, default: '10' },
-         'page': { required: false, default: '0' } 
-        }
+        UserId: { required: true },
+        per_page: { required: false, default: '10' },
+        page: { required: false, default: '0' },
+      },
     }),
 
     wallets: httpMethod({
       method: 'GET',
       path: '{UserId}/wallets',
-      params: { 'UserId': { required: true } }
+      params: { UserId: { required: true } },
     }),
 
     transactions: httpMethod({
       method: 'GET',
-      path: '{UserId}/transactions',
-      params: { 'UserId': { required: true } }
+      path: '{UserId}/transactions?per_page={per_page}&page={page}',
+      params: {
+        UserId: { required: true },
+        per_page: { required: false, default: '10' },
+        page: { required: false, default: '0' },
+      },
     }),
 
     banks: httpMethod({
       method: 'GET',
       path: '{UserId}/bankaccounts',
-      params: { 'UserId': { required: true } }
+      params: { UserId: { required: true } },
     }),
 
     mandates: httpMethod({
       method: 'GET',
       path: '{UserId}/mandates',
-      params: { 'UserId': { required: true } }
+      params: { UserId: { required: true } },
     }),
-
-  }
-
+  },
 })
